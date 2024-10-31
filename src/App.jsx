0f3b4be0fe4f45d286306fdc72cd0ca0759ccb3f
Salutah <salutahmoha@gmail.com>
2024-10-31 15:25:16 +0300

    Building a landing page for BlogIt Blogging platform

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar/navbar';
+import HeroSection from './components/Hero-section/hero';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import './navbar.css';
+
+// function navbar() {
+//   return (
+//     <div className='navbar'>
+//         <div className="log">
+//             <h3>BlogIt</h3>
+//         </div>
+//         <div className="signIn-button">
+//             <button className='btn'>Sign In</button>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default navbar
