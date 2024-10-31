4ac5f673ce2ce7e670e03f26c06111329c6bbf78
Salutah <salutahmoha@gmail.com>
2024-10-31 15:26:53 +0300

    Building a navbar.jsx file

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,17 +1,17 @@
-// import React from 'react'
-// import './navbar.css';
+import React from 'react'
+import './navbar.css';
 
-// function navbar() {
-//   return (
-//     <div className='navbar'>
-//         <div className="log">
-//             <h3>BlogIt</h3>
-//         </div>
-//         <div className="signIn-button">
-//             <button className='btn'>Sign In</button>
-//         </div>
-//     </div>
-//   )
-// }
+function navbar() {
+  return (
+    <div className='navbar'>
+        <div className="log">
+            <h3>BlogIt</h3>
+        </div>
+        <div className="signIn-button">
+            <button className='btn'>Sign In</button>
+        </div>
+    </div>
+  )
+}
 
-// export default navbar
+export default navbar
