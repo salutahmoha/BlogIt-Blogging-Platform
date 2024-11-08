ab9d945c3307d3280ff7639333d1273379e4fef9
Salutah <salutahmoha@gmail.com>
2024-11-08 16:38:55 +0300

    Building a BlogPage where the blogs that belongs to a current user appears only

--- src/pages/Blog Page/BlogPage.jsx
+++ src/pages/Blog Page/BlogPage.jsx
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import Navbar from '../BlogNavbar/navbar';
-// import PersonalBlogsPreview from '../../components/Previews/PersonalBlogsPreview';
-// import PersonalBlogPreview from '../../components/Previews/PersonalBlogPreview';
-// import './BlogPage.css';
+import React from 'react';
+import Navbar from '../BlogNavbar/navbar';
+import PersonalBlogsPreview from '../../components/Previews/PersonalBlogsPreview';
+import PersonalBlogPreview from '../../components/Previews/PersonalBlogPreview';
+import './BlogPage.css';
 
-// function BlogPage() {
-//     return (
-//         <div>
-//             <Navbar />
-//             <PersonalBlogsPreview />
-//         </div>
-//     );
-// }
+function BlogPage() {
+    return (
+        <div>
+            <Navbar />
+            <PersonalBlogsPreview />
+        </div>
+    );
+}
 
-// export default BlogPage;
+export default BlogPage;
