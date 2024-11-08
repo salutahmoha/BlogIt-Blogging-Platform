b21635d31ec56984dcec67776602fbcd8fce7e8f
Salutah <salutahmoha@gmail.com>
2024-11-08 16:32:14 +0300

    Building a profile page where the user can check their profile and update them

--- src/pages/Profile Page/Profile.jsx
+++ src/pages/Profile Page/Profile.jsx
@@ -1,17 +1,17 @@
-// import React from 'react'
-// import UpdatePersonaInformation from '../../components/Previews/UpdatePersonaInformation'
-// import Navbar from '../BlogNavbar/navbar'
-// import UpdatePassword from '../../components/Previews/UpdatePassword'
-// function Profile() {
-//   return (
-//     <div>
-//         <Navbar />
-//         <div className='profile'>
-//         <UpdatePersonaInformation />
-//         <UpdatePassword />
-//         </div>
-//     </div>
-//   )
-// }
+import React from 'react'
+import UpdatePersonaInformation from '../../components/Previews/UpdatePersonaInformation'
+import Navbar from '../BlogNavbar/navbar'
+import UpdatePassword from '../../components/Previews/UpdatePassword'
+function Profile() {
+  return (
+    <div>
+        <Navbar />
+        <div className='profile'>
+        <UpdatePersonaInformation />
+        <UpdatePassword />
+        </div>
+    </div>
+  )
+}
 
-// export default Profile
+export default Profile
